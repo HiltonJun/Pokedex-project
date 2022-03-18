978b14d536c309fdcf8a1467a1c1bec1d6f7c435
HiltonJun <hilton.honda@gmail.com>
2022-03-18 19:43:47 -0300

    heroku prep

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 const express = require("express");
 const path = require("path");
+const port = process.env.PORT || 5000;
 
 const app = express();
-const port = (5000);
 
 app.set("view engine", "ejs");
 app.use(express.static(path.join(__dirname, "public")));
